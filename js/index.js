43a4b0d096b9419e1af3b9771da81c3de1581378
akemi-posi <a.chikaoka@gmail.com>
2025-05-17 10:06:25 +0900

    問題パート

--- js/index.js
+++ js/index.js
@@ -7,21 +7,31 @@ $(document).ready(function () {
     { word: "えんぴつ", kana: "え", img: "img/img_e/enpitsu.png" },
     { word: "おにぎり", kana: "お", img: "img/img_e/onigiri.png" },
   ];
-  //配列の中は[A, B, C]で囲む 文字列の場合["りんご", "いちご", "バナナ"]
-  //それをさらにグループ化{ word:"____", kana:"____", img:"相対パスで場所を表示"}
-  // console.log(cards);
+      //配列の中は[A, B, C]で囲む 文字列の場合["りんご", "いちご", "バナナ"]
+      //それをさらにグループ化{ word:"____", kana:"____", img:"相対パスで場所を表示"}
+      // console.log(cards);
 
   let correctKana = ""; //最初は見えないようにしておく
 
-  //問題のランダム処理
+  //問題
   function setNewQuestion() {
+    //ランダム処理
     const random = cards[Math.floor(Math.random() * cards.length)];
-    //console.log(random);
+        //console.log(random);
     correctKana = random.kana;
-    //console.log(correctKana);
+        //console.log(correctKana);
+
+    //絵をrandomのimgに替える cf.attribute-属性
+    $(".card_wrap img").attr("src", random.img);
+       //.card_wrap imgのsrcをrandom.imgに替える
+
+    //wordの最初の文字を飛ばして2文字目(1)から取り出す
+    const restOfWord = random.word.substring(1);
+       //console.log(restOfWord);
+
+    //restOfWordの前に?をつけて.question_wordに表示
+    $(".question_word").html(`<span class="hatena">？</span>${restOfWord}`);
   }
 
-
-  
   setNewQuestion();
 });
