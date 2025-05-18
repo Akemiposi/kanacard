808124409c995f1378fd737eec5952c9f53dad3d
Akemiposi <a.chikaoka@gmail.com>
2025-05-18 20:22:27 +0900

    img追加終了

--- js/index.js
+++ js/index.js
@@ -1,69 +1,71 @@
 //単語とかなと絵を組み合わせて配列にする
 $(document).ready(function () {
   const allCards = {
-    a: [
-      { word: "あさがお", kana: "あ", img: "img/img_e/asagao.png" },
-      { word: "いぬ", kana: "い", img: "img/img_e/inu.png" },
-      { word: "うさぎ", kana: "う", img: "img/img_e/usagi.png" },
-      { word: "えんぴつ", kana: "え", img: "img/img_e/enpitsu.png" },
-      { word: "おにぎり", kana: "お", img: "img/img_e/onigiri.png" },
-    ],
-    ka: [
-      { word: "あさがお", kana: "か", img: "img/img_e/asagao.png" },
-      { word: "きつね", kana: "き", img: "img/img_e/kitsune.png" },
-      { word: "くま", kana: "く", img: "img/img_e/kuma.png" },
-      { word: "えんぴつ", kana: "け", img: "img/img_e/enpitsu.png" },
-      { word: "おにぎり", kana: "こ", img: "img/img_e/onigiri.png" },
-    ],
-    sa: [
-      { word: "あさがお", kana: "さ", img: "img/img_e/asagao.png" },
-      { word: "いぬ", kana: "し", img: "img/img_e/inu.png" },
-      { word: "うさぎ", kana: "す", img: "img/img_e/usagi.png" },
-      { word: "えんぴつ", kana: "せ", img: "img/img_e/enpitsu.png" },
-      { word: "おにぎり", kana: "そ", img: "img/img_e/onigiri.png" },
-    ],
-    ta: [
-      { word: "あさがお", kana: "た", img: "img/img_e/asagao.png" },
-      { word: "いぬ", kana: "ち", img: "img/img_e/inu.png" },
-      { word: "つくえ", kana: "つ", img: "img/img_e/tsukue.png" },
-      { word: "えんぴつ", kana: "て", img: "img/img_e/enpitsu.png" },
-      { word: "おにぎり", kana: "と", img: "img/img_e/onigiri.png" },
-    ],
-    na: [
-      { word: "あさがお", kana: "な", img: "img/img_e/asagao.png" },
-      { word: "いぬ", kana: "に", img: "img/img_e/inu.png" },
-      { word: "うさぎ", kana: "ぬ", img: "img/img_e/usagi.png" },
-      { word: "えんぴつ", kana: "ね", img: "img/img_e/enpitsu.png" },
-      { word: "おにぎり", kana: "の", img: "img/img_e/onigiri.png" },
-    ],
-    ha: [
-      { word: "あさがお", kana: "は", img: "img/img_e/asagao.png" },
-      { word: "いぬ", kana: "ひ", img: "img/img_e/inu.png" },
-      { word: "うさぎ", kana: "ふ", img: "img/img_e/usagi.png" },
-      { word: "えんぴつ", kana: "へ", img: "img/img_e/enpitsu.png" },
-      { word: "おにぎり", kana: "ほ", img: "img/img_e/onigiri.png" },
-    ],
-    ma: [
-      { word: "あさがお", kana: "ま", img: "img/img_e/asagao.png" },
-      { word: "いぬ", kana: "み", img: "img/img_e/inu.png" },
-      { word: "うさぎ", kana: "む", img: "img/img_e/usagi.png" },
-      { word: "えんぴつ", kana: "め", img: "img/img_e/enpitsu.png" },
-      { word: "おにぎり", kana: "も", img: "img/img_e/onigiri.png" },
-    ],
-    ya: [
-      { word: "あさがお", kana: "や", img: "img/img_e/asagao.png" },
-      { word: "いぬ", kana: "ゆ", img: "img/img_e/inu.png" },
-      { word: "うさぎ", kana: "よ", img: "img/img_e/usagi.png" },
-    ],
-    ra: [
-      { word: "あさがお", kana: "ら", img: "img/img_e/asagao.png" },
-      { word: "いぬ", kana: "り", img: "img/img_e/inu.png" },
-      { word: "うさぎ", kana: "る", img: "img/img_e/usagi.png" },
-      { word: "えんぴつ", kana: "れ", img: "img/img_e/enpitsu.png" },
-      { word: "おにぎり", kana: "ろ", img: "img/img_e/onigiri.png" },
-    ],
-    wa: [{ word: "あさがお", kana: "わ", img: "img/img_e/asagao.png" }],
-  };
+  a: [
+    { word: "あさがお", kana: "あ", img: "img/img_e/asagao.png" },
+    { word: "いぬ", kana: "い", img: "img/img_e/inu.png" },
+    { word: "うさぎ", kana: "う", img: "img/img_e/usagi.png" },
+    { word: "えのぐ", kana: "え", img: "img/img_e/enogu.png" },
+    { word: "おにぎり", kana: "お", img: "img/img_e/onigiri.png" }
+  ],
+  ka: [
+    { word: "かぶとむし", kana: "か", img: "img/img_e/kabutomushi.png" },
+    { word: "きつね", kana: "き", img: "img/img_e/kitsune.png" },
+    { word: "くま", kana: "く", img: "img/img_e/kuma.png" },
+    { word: "けしごむ", kana: "け", img: "img/img_e/keshigomu.png" },
+    { word: "こくばん", kana: "こ", img: "img/img_e/kokuban.png" }
+  ],
+  sa: [
+    { word: "さい", kana: "さ", img: "img/img_e/sai.png" },
+    { word: "しんかんせん", kana: "し", img: "img/img_e/shinkansen.png" },
+    { word: "すいか", kana: "す", img: "img/img_e/suika.png" },
+    { word: "せみ", kana: "せ", img: "img/img_e/semi.png" },
+    { word: "そら", kana: "そ", img: "img/img_e/sora.png" }
+  ],
+  ta: [
+    { word: "たまご", kana: "た", img: "img/img_e/tamago.png" },
+    { word: "ちず", kana: "ち", img: "img/img_e/chizu.png" },
+    { word: "つくえ", kana: "つ", img: "img/img_e/tsukue.png" },
+    { word: "てんとうむし", kana: "て", img: "img/img_e/tentoumushi.png" },
+    { word: "とけい", kana: "と", img: "img/img_e/tokei.png" }
+  ],
+  na: [
+    { word: "なわとび", kana: "な", img: "img/img_e/nawatobi.png" },
+    { word: "にんじん", kana: "に", img: "img/img_e/ninjin.png" },
+    { word: "ぬいぐるみ", kana: "ぬ", img: "img/img_e/nuigurumi.png" },
+    { word: "ねこ", kana: "ね", img: "img/img_e/neko.png" },
+    { word: "のり", kana: "の", img: "img/img_e/nori.png" }
+  ],
+  ha: [
+    { word: "はさみ", kana: "は", img: "img/img_e/hasami.png" },
+    { word: "ひこうき", kana: "ひ", img: "img/img_e/hikouki.png" },
+    { word: "ふうせん", kana: "ふ", img: "img/img_e/fuusen.png" },
+    { word: "へちま", kana: "へ", img: "img/img_e/hechima.png" },
+    { word: "ほし", kana: "ほ", img: "img/img_e/hoshi.png" }
+  ],
+  ma: [
+    { word: "まめ", kana: "ま", img: "img/img_e/mame.png" },
+    { word: "みかん", kana: "み", img: "img/img_e/mikan.png" },
+    { word: "むしめがね", kana: "む", img: "img/img_e/mushimegane.png" },
+    { word: "めがね", kana: "め", img: "img/img_e/megane.png" },
+    { word: "もも", kana: "も", img: "img/img_e/momo.png" }
+  ],
+  ya: [
+    { word: "やさい", kana: "や", img: "img/img_e/yasai.png" },
+    { word: "ゆびわ", kana: "ゆ", img: "img/img_e/yubiwa.png" },
+    { word: "ようふく", kana: "よ", img: "img/img_e/youfuku.png" }
+  ],
+  ra: [
+    { word: "らくだ", kana: "ら", img: "img/img_e/rakuda.png" },
+    { word: "りす", kana: "り", img: "img/img_e/risu.png" },
+    { word: "るすばん", kana: "る", img: "img/img_e/rusuban.png" },
+    { word: "れいぞうこ", kana: "れ", img: "img/img_e/reizouko.png" },
+    { word: "ろうそく", kana: "ろ", img: "img/img_e/rousoku.png" }
+  ],
+  wa: [
+    { word: "わに", kana: "わ", img: "img/img_e/wani.png" }
+  ]
+};
   //配列の中は[A, B, C]で囲む 文字列の場合["りんご", "いちご", "バナナ"]
   //それをさらにグループ化{ word:"____", kana:"____", img:"相対パスで場所を表示"}
   // console.log(cards);
